3b55a816a7a688586c2c107369e4d4821e038a1e
gutierrezjuanmanuel <gutierrezmanueljuan@outlook.com>
2023-10-07 18:22:46 +0000

    cambio

---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CartWidget from '../CartWidget/CartWidget';
+import './NavBar.css' ;
+
+const NavBar = () => {
+  return (
+    <header>
+<h1> Casa de la música</h1>
+<nav>
+  <ul>
+    <li>Guitarras</li>
+    <li>Bajos</li>
+    <li>Baterias</li>
+  </ul>
+</nav>
+<CartWidget/>
+    </header>
+  )
+}
+
+export default NavBar
